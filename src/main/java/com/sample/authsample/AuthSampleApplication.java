7b332452387a173729a1e97bea71c7581b58333c
marden.paiva <mardensp@hotmail.com>
2022-09-26 08:49:45 -0300

    Update AuthSampleApplication.java

--- src/main/java/com/sample/authsample/AuthSampleApplication.java
+++ src/main/java/com/sample/authsample/AuthSampleApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthSampleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Trigger hook");
+		
 		SpringApplication.run(AuthSampleApplication.class, args);
 	}
 
